eeadc6a7846126c7e6064db769ac5c36d8a2b810
Kashish Jain <154213743+j-kashish@users.noreply.github.com>
2025-09-14 19:20:18 +0530

    Fixed MIME-Type Mismatch

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import "./style.css";
-import products from "./api/products.json";
 import { showProductContainer } from "./homeProductCards.js";
 
-// Define a function named `showProductContainer` that takes an array of products as input.
-showProductContainer(products);
-
-//todo Don't Forget To LIKE SHARE & SUBSCRIBE TO THAPA TECHNCIAL YOUTUBE CHANNEL 👉 https://www.youtube.com/thapatechnical
+fetch("./api/products.json")
+  .then((response) => response.json())
+  .then((products) => {
+    showProductContainer(products);
+  })
+  .catch((err) => console.error("Failed to load products:", err));
